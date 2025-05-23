61889f6bfe3a0e66704d82a843e29788e5763026
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-05-23 17:19:57 +1000

    https://github.com/mP1/walkingkooka-spreadsheet/pull/6579 SpreadsheetMetadata.dateTimeContext Optional<SpreadsheetCell> added
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/6579
    - SpreadsheetMetadata.dateTimeContext Optional<SpreadsheetCell> added
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/6580
    - SpreadsheetMetadata.spreadsheetFormatterContext Optional<SpreadsheetCell> was required

--- src/test/java/walkingkooka/spreadsheet/server/platform/JettyHttpServerSpreadsheetHttpServerTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/platform/JettyHttpServerSpreadsheetHttpServerTest.java
@@ -38,6 +38,7 @@ public final class JettyHttpServerSpreadsheetHttpServerTest implements PublicSta
         );
 
         metadata.spreadsheetConverterContext(
+            SpreadsheetMetadata.NO_CELL,
             SpreadsheetConverterContexts.NO_METADATA,
             SpreadsheetConverterContexts.NO_VALIDATION_REFERENCE,
             SpreadsheetMetadataPropertyName.FORMULA_CONVERTER,
